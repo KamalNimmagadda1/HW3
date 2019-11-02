4d0bf98e9d01420bb1cca846321d57a8f652aec0
KamalNimmagadda1 <55113221+KamalNimmagadda1@users.noreply.github.com>
2019-11-01 19:49:36 -0700

    Update collision.py

--- code/collision.py
+++ code/collision.py
@@ -14,3 +14,24 @@ def line_sphere_intersection(p1, p2, c, r):
         intersection formula, as a np.float64 scalar
     """
     # FILL in your code here
+    
+    l = p1 - p2
+    s = 0
+    for i in range(len(l)):
+	s += np.power(l[i], 2)
+    mag = np.power(s, 0.5)
+    for i in range(len(l)):
+	l[i]= (l[i]/mag)
+    a1 = c - p1
+    a = np.dot(l, a1)
+    b1 = np.dot(a1, a1)
+    b = b1 - np.power(r, 2)
+    d = np.power(a, 2) - b
+    return -d
+
+p1 = np.array([0.0, 0.0, 0.0])
+p2 = np.array([0.7, 0.0, 0.0])
+c = np.array([1.0,0.3,0.0])
+r = 0.2
+d = line_sphere_intersection(p1, p2, c, r)
+print(d)
